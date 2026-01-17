33151c0debe94eb4444bf41267dc6a53d706e5ab
Joshua LaBelle <labelljd@gmail.com>
2026-01-17 10:19:53 -0500

    fix rotate wallpaper script

--- scripts/rotate_wallpaper.sh
+++ scripts/rotate_wallpaper.sh
@@ -5,13 +5,14 @@
 # Open your crontab with `crontab -e`
 
 # select a random wallpaper from the directory
-RANDOM_FILE=$(ls /home/josh/Pictures/wallpapers/ | xargs shuf -e | head -n1)
+RANDOM_FILE=$(find /home/josh/Pictures/wallpapers/ -type f -print0 | xargs -0 shuf -e | head -n1)
 
 # set environment variable so cron can run the command correctly
 # cron has a very restricted set of ENV vars that it uses, so alot of times
 # you have to explicitly set them.
 PID=$(pgrep gnome-session | head -n1)
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/"$PID"/environ|cut -d= -f2-)
+export DBUS_SESSION_BUS_ADDRESS
 
 # run the command to change the background wallpaper
 gsettings set org.gnome.desktop.background picture-uri-dark "file:///home/josh/Pictures/wallpapers/$RANDOM_FILE"
